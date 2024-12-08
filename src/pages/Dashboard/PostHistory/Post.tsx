d405872bef6f67a358a5c1cc402f969baf042c41
DrkNowak <dariusznowak194@gmail.com>
2024-12-09 00:14:55 +0100

    feat: post

--- src/pages/Dashboard/PostHistory/Post.tsx
+++ src/pages/Dashboard/PostHistory/Post.tsx
@@ -1,10 +1,25 @@
-function PostHistory({ tweet }: { tweet: Tweet }) {
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import UserAvatar from '../../../components/UserAvatar/UserAvatar';
+
+import GlobalStyles from '../../../ui-kit/GlobalStyles';
+import PostHistoryStyles from './PostHistoryStyles';
+
+import { Tweet } from "../../../types/types";
+
+function Post({ post }: { post: Tweet }) {
+    const styles = { ...GlobalStyles(), ...PostHistoryStyles() };
+
     return (
-        <Box>
-         {history}
-        </Box>
+        <Box sx={{ ...styles.borderBox, ...styles.postWrapper }} key={post.id}>
+           <UserAvatar user={{name: post.name} }/>
+            <Box>
+                <Typography variant="caption" sx={{textTransform: 'capitalize'}}>{post.name}</Typography>
+                <Typography variant="body1" dangerouslySetInnerHTML={{__html: post.text}}/>
+            </Box>  
+        </Box>)
     );
 }
 
 
-export default memo(PostHistory);
+export default Post;
--- src/pages/Dashboard/PostHistory/PostHistory.tsx
+++ src/pages/Dashboard/PostHistory/PostHistory.tsx
@@ -1,28 +1,15 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-
-import UserAvatar from '../../../components/UserAvatar/UserAvatar';
-import GlobalStyles from '../../../ui-kit/GlobalStyles';
-import PostHistoryStyles from './PostHistoryStyles';
-import { Tweet } from "../../../types/types";
 import { memo } from 'react';
 
-function PostHistory({tweets}: {tweets: Tweet[]}){
-    const styles = { ...GlobalStyles(), ...PostHistoryStyles() };
+import Post from './Post';
+import { Tweet } from "../../../types/types";
 
-    const history = tweets.map(tweet => 
-        <Box sx={{ ...styles.borderBox, ...styles.postWrapper }} key={tweet.id}>
-             <UserAvatar user={{name: tweet.name} }/>
-            <Box>
-                <Typography variant="caption" sx={{textTransform: 'capitalize'}}>{tweet.name}</Typography>
-                <Typography variant="body1" dangerouslySetInnerHTML={{__html: tweet.text}}/>
-            </Box>
-        </Box>).reverse();
+function PostHistory({tweets}: {tweets: Tweet[]}){
+  
+
+    const history = tweets.map((tweet: Tweet) => <Post post={tweet} key={tweet.id} />).reverse();
 
     return (
-       <Box>
-        {history}
-       </Box>
+       <>{history}</>
     );
 }
 
